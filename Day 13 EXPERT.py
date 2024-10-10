99852ac98a304b6bb939fa931680cf81b046429a
saloni sharma <145436884+Salonosharma2005@users.noreply.github.com>
2024-10-10 20:05:19 +0530

    Add files via upload

---
+++ Day 13 EXPERT.py
@@ -0,0 +1,11 @@
+# cook your dish here
+T = int(input())
+
+for _ in range(T):
+    
+    X, Y = map(int, input().split())
+    
+    if 2 * Y >= X:
+        print("YES")
+    else:
+        print("NO")
