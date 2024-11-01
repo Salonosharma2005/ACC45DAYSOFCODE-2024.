d3071198fc780d3813473ff37d63923e57da614d
saloni sharma <145436884+Salonosharma2005@users.noreply.github.com>
2024-11-01 21:48:58 +0530

    Add files via upload

---
+++ Day 30 TRUESCORE.py
@@ -0,0 +1,12 @@
+# cook your dish here
+T = int(input())
+
+for _ in range(T):
+   
+    A, B = map(int, input().split())
+    C, D = map(int, input().split())
+    
+    if C >= A and D >= B:
+        print("POSSIBLE")
+    else:
+        print("IMPOSSIBLE")
